7f751b5cba2005a18bffd9eb04290c28d5d94655
Niki Dokovski <nikolai.dokovski@sap.com>
2021-03-28 21:30:03 +0200

    adding nginx access filebeat

--- install-ingress-nginx.sh
+++ install-ingress-nginx.sh
@@ -8,7 +8,9 @@ echo "setting up ingress nginx controller stack"
 
 kubectl create namespace ingress-nginx \
   --dry-run=client -o yaml | kubectl apply -f -
-      
+
+kubectl apply -f $dir/ingress-nginx/filebeat-nginx-configmap.yaml
+
 helm upgrade ingress-nginx ingress-nginx/ingress-nginx \
   -n ingress-nginx -f $dir/ingress-nginx/ingress-nginx-values.yaml \
   --install --wait --timeout 15m

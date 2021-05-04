1953c09ef356bddb59bf351cce821b599d812d54
Niki Dokovski <nikolai.dokovski@sap.com>
2021-05-04 20:58:21 +0200

    clean up

--- install-ingress-nginx.sh
+++ install-ingress-nginx.sh
@@ -9,18 +9,8 @@ echo "setting up ingress nginx controller stack"
 kubectl create namespace ingress-nginx \
   --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl apply -f $dir/ingress-nginx/filebeat-nginx-configmap.yaml
+kubectl apply -f $dir/ingress-nginx/filebeat-nginx-configmap.yaml -n ingress-nginx
 
 helm upgrade ingress-nginx ingress-nginx/ingress-nginx \
   -n ingress-nginx -f $dir/ingress-nginx/ingress-nginx-values.yaml \
   --install --wait --timeout 15m
-
-
-for var in "$@"
-do
-    if [[ "$var" = "--with-coredns-patch" ]]; then
-      echo "patching coredns"
-      $dir/coredns/patch_coredns.sh
-    fi  
-
-done

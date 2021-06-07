e61b7d7a6280dcb0892373cfe0a86fc1562ca041
Niki Dokovski <nikolai.dokovski@sap.com>
2021-06-07 23:35:34 +0200

    remove filebeat

--- setup.sh
+++ setup.sh
@@ -13,8 +13,6 @@ echo "setting up ingress nginx controller stack"
 kubectl create namespace ingress-nginx \
   --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl apply -f $dir/filebeat-nginx-configmap.yaml -n ingress-nginx
-
 helm upgrade ingress-nginx ingress-nginx/ingress-nginx \
   -n ingress-nginx -f $dir/ingress-nginx-values.yaml \
   --install --wait --timeout 15m
